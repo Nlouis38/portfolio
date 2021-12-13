4083ef41588f02f86c461a1546a225d6a5af1f33
Nazir Louis <nazirlouis@Nazirs-MacBook-Pro.local>
2021-12-13 17:40:22 -0500

    Ladning

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,15 +1,21 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import "./Landing.css";
 
 function Landing() {
   return (
     <Container>
       <MainText>
-        <Name>Nazir Louis</Name>
-        <SubHeading>Robotics Engineer Turned Software Engineer</SubHeading>
-        <About>
-          ksjaf sajf kjsad fkjdsa ;kfjkdsajf kdjsa f;jkds;ajfk;sadj f;kjsad
-          kf;jdsakfjksd;alj fkadjs kf;ldjsa f;kjdas k;fjdsaklf jadsk
+        <Name className="text">Nazir Louis</Name>
+        <SubHeading className="text">
+          Robotics Engineer Turned Software Engineer
+        </SubHeading>
+        <About className="text">
+          Sarah watched the whirlpool mesmerized. She couldn't take her eyes off
+          the water swirling around and around. She stuck in small twigs and
+          leaves to watch the whirlpool catch them and then suck them down. It
+          bothered her more than a little bit that this could also be used as a
+          metaphor for her life.
         </About>
       </MainText>
     </Container>
@@ -19,7 +25,7 @@ function Landing() {
 export default Landing;
 
 const Container = styled.div`
-  padding: 20px;
+  display: flex;
   height: 100vh;
   width: 100vw;
   background-color: gray;
@@ -31,18 +37,20 @@ const MainText = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  max-width: 50vw;
+  margin: 50px;
 `;
 
 const Name = styled.div`
-  font-size: 32px;
+  font-size: 16px;
 `;
 
 const SubHeading = styled.div`
-  font-size: 24px;
+  font-size: 10px;
+  color: white;
 `;
 
 const About = styled.div`
   display: flex;
-  font-size: 16px;
+  font-size: 8px;
 `;
