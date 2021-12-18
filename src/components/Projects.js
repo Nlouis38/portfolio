87c65eb36b861bdaba06f2c73d338eafcc963e7e
Nazir Louis <nazirlouis38@gmail.com>
2021-12-17 21:35:45 -0500

    Starting on Project Page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,48 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
+import { Skeleton } from "@mui/material";
 
 function Projects() {
   return (
     <Container>
-      <h1>YOOOO</h1>
+      <Text>
+        <Skeleton
+          className="text"
+          variant="rectangular"
+          width={400}
+          height={50}
+        />
+        <Skeleton
+          className="text"
+          variant="rectangular"
+          width={400}
+          height={300}
+        />
+        <Buttons>
+          <SourceBtn>Source Code</SourceBtn>
+          <DemoBtn>Demo</DemoBtn>
+        </Buttons>
+      </Text>
+      <Content>
+        <Skeleton
+          className="image"
+          variant="rectangular"
+          width={300}
+          height={300}
+        />
+        <Skeleton
+          className="image"
+          variant="rectangular"
+          width={300}
+          height={300}
+        />
+        <Skeleton
+          className="image"
+          variant="rectangular"
+          width={300}
+          height={300}
+        />
+      </Content>
     </Container>
   );
 }
@@ -17,4 +55,63 @@ const Container = styled.div`
   color: white;
   max-width: 100%;
   overflow-x: hidden;
+  background-color: red;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Text = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  .text {
+    margin: 10px;
+  }
+`;
+
+const Buttons = styled.div`
+  color: black;
+  display: flex;
+  justify-content: center;
+  width: 420px;
+  font-family: "Bakbak One", cursive;
+  font-size: 22px;
+`;
+
+const SourceBtn = styled.div`
+  background-color: white;
+  margin: 10px;
+  width: 200px;
+  height: 40px;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const DemoBtn = styled.div`
+  background-color: white;
+  margin: 10px;
+  width: 180px;
+  height: 40px;
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .image {
+    margin: 10px;
+  }
 `;
