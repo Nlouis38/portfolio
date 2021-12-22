6526d58b3d949c8c35bfdc928a2bd06506c81fb6
Nazir Louis <nazirlouis38@gmail.com>
2021-12-22 17:51:55 -0500

    nice

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       <Skills />
       <Projects
         title="Tesla Clone"
-        details="This project was used to practice my skills in copying an exact version of a website. 
+        details="This project was used to practice my skills in copying an exact version of a website.
         I was able to do this using React with styled components. Props were used for most of the components
         so I did not have to remake the same component multiple times, this is one of the benefits of React!"
         image1={TeslaImage}
