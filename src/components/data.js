b12368ece9f55cc97408b50ef835064c5557818d
Nazir Louis <nazirlouis38@gmail.com>
2022-01-25 20:02:49 -0500

    Re-doing projects

---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+const projects = [
+  {
+    id: 1,
+    title: "Tesla Clone",
+    demo: "https://61c3985f4bd53f988b189ed1--boring-ritchie-aede20.netlify.app/",
+    source: "https://github.com/Nlouis38/tesla-clone",
+    img: "../images/TeslaImage.png",
+    category: "favorites",
+  },
+];
