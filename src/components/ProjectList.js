c446fd4833b6cf657dc1fcdb3deb554686095b39
Nazir Louis <nazirlouis38@gmail.com>
2022-01-25 20:43:27 -0500

    Working on button links

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,7 +3,24 @@ import data from "./data";
 
 function ProjectList() {
   const [project, setProjects] = useState(data);
-  return <div>{console.log(data)}</div>;
+  return (
+    <div>
+      {data.map((project) => {
+        const { id, title, demo, source, img, category } = project;
+        console.log(title);
+        return (
+          <div className="projects" key={id}>
+            <h1>{title}</h1>
+            <img src={img} alt={title} />
+            <div className="btn-container">
+              <button className="demo">Demo</button>
+              <button className="source">Source</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ProjectList;
