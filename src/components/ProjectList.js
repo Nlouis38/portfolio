248a943351d5e071a965b31c08fcf5ed6baf6340
Nazir Louis <nazirlouis38@gmail.com>
2022-01-26 22:13:32 -0500

    Menu functionality is working just need to make it look pretty

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,20 +2,57 @@ import React, { useState } from "react";
 import data from "./data";
 import styled from "styled-components";
 
+const allCategories = [
+  "All",
+  ...new Set(
+    data.map((project) => {
+      return project.category;
+    })
+  ),
+];
+
+console.log(allCategories);
 function ProjectList() {
-  const [project, setProjects] = useState(data);
+  const [projects, setProjects] = useState(data);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterProjects = (category) => {
+    console.log(category);
+    if (category === "All") {
+      setProjects(data);
+    } else {
+      const newProjectList = data.filter(
+        (project) => project.category === category
+      );
+      setProjects(newProjectList);
+      console.log(newProjectList);
+    }
+  };
+
   return (
     <Container>
-      {data.map((project) => {
+      <BtnContainer>
+        {allCategories.map((category, index) => {
+          return (
+            <button key={index} onClick={() => filterProjects(category)}>
+              {category}
+            </button>
+          );
+        })}
+      </BtnContainer>
+      {projects.map((project) => {
         const { id, title, demo, source, img, category } = project;
-        console.log(title);
         return (
           <div className="projects" key={id}>
             <h1>{title}</h1>
             <img src={img} alt={title} />
             <div className="btn-container">
-              <button className="demo">Demo</button>
-              <button className="source">Source</button>
+              <a href={demo}>
+                <button className="demo">Demo</button>
+              </a>
+              <a href={source}>
+                <button className="source">Source</button>
+              </a>
             </div>
           </div>
         );
