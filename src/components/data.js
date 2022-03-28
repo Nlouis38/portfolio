727d918be76d7571508e9323a5cc3bfd53c2bf7c
Nazir Louis <nazirlouis38@gmail.com>
2022-03-28 13:32:25 -0400

    Start of making portfolio Data Anal Based

--- src/components/data.js
+++ src/components/data.js
@@ -1,62 +1,6 @@
-<<<<<<< HEAD
-import PortfolioImage from "../images/Pixel3.jpg";
-import OpenCV from "../images/OpenCV.png";
-=======
-import TeslaImage from "../images/TeslaVideo.gif";
 import PortfolioImage from "../images/Pixel1.jpg";
->>>>>>> e7460d90acedaa4367c95df6db1f53e3d2d6e91e
 
 const projects = [
-  {
-    id: 1,
-<<<<<<< HEAD
-    title: "Digital Waterfall (C++)",
-    demo: "",
-    source: "https://github.com/Nlouis38/DigitalWaterFall",
-    img: OpenCV,
-    category: "High-Level Robotics",
-=======
-    title: "Tesla Clone",
-    demo: "https://61c3985f4bd53f988b189ed1--boring-ritchie-aede20.netlify.app/",
-    source: "https://github.com/Nlouis38/tesla-clone",
-    img: TeslaImage,
-    category: "Web Development",
->>>>>>> e7460d90acedaa4367c95df6db1f53e3d2d6e91e
-  },
-  {
-    id: 2,
-    title: "Auto Object Measurement (Python)",
-    demo: "",
-<<<<<<< HEAD
-    source: "https://github.com/Nlouis38/OpjectMeasure_1",
-    img: OpenCV,
-    category: "Machine Learning/AI",
-=======
-    source: "https://github.com/Nlouis38/BlackJack",
-    img: "https://en.wikipedia.org/wiki/File:Blackjack21.jpg",
-    category: "Games",
->>>>>>> e7460d90acedaa4367c95df6db1f53e3d2d6e91e
-  },
-
-  {
-    id: 3,
-<<<<<<< HEAD
-    title: "My Portfolio (React)",
-=======
-    title: "Smart Door Locks",
-    demo: "",
-    source: "https://github.com/Nlouis38/01-birthday-reminder",
-    img: "",
-    category: "Arduino Automation",
-  },
-  {
-    id: 4,
-    title: "Staple Gun Cycling",
-    demo: "",
-    source: "https://github.com/Nlouis38/01-birthday-reminder",
-    img: "",
-    category: "High-Level Automation",
-  },
   {
     id: 5,
     title: "Digital Waterfall",
@@ -76,7 +20,6 @@ const projects = [
   {
     id: 7,
     title: "My Portfolio",
->>>>>>> e7460d90acedaa4367c95df6db1f53e3d2d6e91e
     demo: "nazirlouis.com",
     source: "https://github.com/Nlouis38/portfolio.git",
     img: PortfolioImage,
