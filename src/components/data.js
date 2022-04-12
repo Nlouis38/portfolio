8918a7305fb03af3319df2227d256acd7bb83b9e
Nazir Louis <nazirlouis38@gmail.com>
2022-04-12 16:49:35 -0400

    Images and Excel Added

--- src/components/data.js
+++ src/components/data.js
@@ -1,13 +1,15 @@
 import PortfolioImage from "../images/Pixel1.jpg";
 import StapleImage from "../images/StapleGuns_PLR.PNG";
-import NailsImage from "../images/Nails_PLR.PNG";
+import ScrewImage from "../images/Nails_PLR.PNG";
+import StapleExcel from "../images/StapleExcel.xlsx";
+import ScrewExcel from "../images/ScrewExcel.xlsx";
 
 const projects = [
   {
     id: 1,
     title: "Home Depot Staple Guns PLR",
     demo: "",
-    source: "",
+    source: StapleExcel,
     img: StapleImage,
     category: "Excel",
   },
@@ -15,8 +17,8 @@ const projects = [
     id: 2,
     title: "Home Depot Deckmate Screws PLR",
     demo: "",
-    source: "",
-    img: NailsImage,
+    source: ScrewExcel,
+    img: ScrewImage,
     category: "Excel",
   },
   {
@@ -24,14 +26,15 @@ const projects = [
     title: "Covid-19 Case Studies",
     demo: "",
     source: "",
-    img: "",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg",
     category: "SQL",
   },
   {
     id: 5,
-    title: "Digital Waterfall (Python & C++)",
+    title: "Digital Waterfall (Python)",
     demo: "",
-    source: "https://github.com/Nlouis38/01-birthday-reminder",
+    source:
+      "https://github.com/Nlouis38/Image2Array/blob/main/ImageToVariable2.py",
     img: "https://image.made-in-china.com/2f0j00prMRuKgEqnqT/Round-Shape-Hang-Type-Water-Curtain-Fountain-Digital-LED-Waterfall.jpg",
     category: "Computer Vision",
   },
@@ -39,7 +42,7 @@ const projects = [
     id: 6,
     title: "Object Detection and Measurement (Python)",
     demo: "",
-    source: "https://github.com/Nlouis38/01-birthday-reminder",
+    source: "https://github.com/Nlouis38/OpjectMeasure_1",
     img: "https://miro.medium.com/max/1400/1*S8Il5ethl3YFh0M9XKVz-A.png",
     category: "Computer Vision",
   },
@@ -52,4 +55,5 @@ const projects = [
     category: "Web Development",
   },
 ];
+
 export default projects;
